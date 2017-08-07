9e934fcd55efc150fcd6c0be689da8668639de2e
David Hale and Mark Helt <mark@>
2017-08-07 09:34:00 -0700

    Create basic framework of calculator webpage

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var sub = function(number1, number2) {
+  return number1 - number2;
+};
+
+var mult = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = sub(number1, number2);
+alert(result);
