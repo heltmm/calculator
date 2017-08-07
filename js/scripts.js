d69d4a274efa3936b0ca0dba044701a1ebeb5a0e
David Hale and Mark Helt <david@>
2017-08-07 13:20:33 -0700

    add styling for calculator button

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+//var number1 = parseInt(prompt("Enter a number:"));
+//var number2 = parseInt(prompt("Enter another number:"));
 var result = sub(number1, number2);
-alert(result);
+//alert(result);
