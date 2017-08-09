904adb6f6ba28e028a13dc7f5735ab168e70d09c
Javier Jimenez and Mark Helt <mark@>
2017-08-09 11:53:46 -0700

    Add frond end for add

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business interface
 var add = function(number1, number2) {
   return number1 + number2;
 };
@@ -13,6 +14,19 @@ var mult = function(number1, number2) {
 var divide = function(number1, number2) {
   return number1 / number2;
 };
+
+//User interface
+$(document).ready(function() {
+  $('form#add').submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($('#add1').val());
+    var number2 = parseInt($("#add2").val());
+    var theSumOf = add(number1, number2);
+
+    $(".hiddenAnswer").text(theSumOf);
+    $("#results").show();
+  });
+});
 
 //var number1 = parseInt(prompt("Enter a number:"));
 //var number2 = parseInt(prompt("Enter another number:"));
