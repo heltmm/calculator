3005bbc7cc140030257bc49022655b65e14c60a9
Mark Helt <heltmm@gmail.com>
2017-08-10 17:57:46 -0700

    Relplace the add, sub, div, mult forms with one form with 4 radio buttons, update js to change calculator operator based on button choice.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,11 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-var sub = function(number1, number2) {
+var subbtract = function(number1, number2) {
   return number1 - number2;
 };
 
-var mult = function(number1, number2) {
+var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
@@ -18,43 +18,23 @@ var divide = function(number1, number2) {
 //User interface
 $(document).ready(function() {
   //user interface for addition
-  $('form#add').submit(function(event) {
+  $('form#calc').submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($('#add1').val());
-    var number2 = parseInt($("#add2").val());
-    var theSumOf = add(number1, number2);
+    var number1 = parseInt($('#input1').val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add"){
+      result = add(number1, number2)
+    }else if (operator === "subtract"){
+      result = subbtract(number1, number2)
+    }else if (operator === "multiply"){
+      result = multiply(number1, number2)
+    }else if (operator === "divide"){
+      result = divide(number1, number2)
+    }
 
-    $(".hiddenAnswer").text(theSumOf);
-    $("#results").show();
-  });
-  //user interface for subtraction
-  $('form#sub').submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($('#sub1').val());
-    var number2 = parseInt($("#sub2").val());
-    var theSumOf = sub(number1, number2);
-
-    $(".hiddenAnswer").text(theSumOf);
-    $("#results").show();
-  });
-  //user interface for multiplication
-  $('form#mult').submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($('#mult1').val());
-    var number2 = parseInt($("#mult2").val());
-    var theSumOf = mult(number1, number2);
-
-    $(".hiddenAnswer").text(theSumOf);
-    $("#results").show();
-  });
-  //user interface for division
-  $('form#div').submit(function(event) {
-    event.preventDefault();
-    var number1 = parseInt($('#div1').val());
-    var number2 = parseInt($("#div2").val());
-    var theSumOf = divide(number1, number2);
-
-    $(".hiddenAnswer").text(theSumOf);
+    $(".hiddenAnswer").text(result);
     $("#results").show();
   });
 });
